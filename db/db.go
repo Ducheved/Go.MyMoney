1f2eb004c542b52b784face6237d641c73e83313
MrDucheved <ducheved@mail.ru>
2024-08-08 11:55:25 +0300

    refactor: Remove unused code in db package

--- db/db.go
+++ db/db.go
@@ -1,8 +1,6 @@
 package db
 
 import (
-	"strings"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
@@ -10,10 +8,6 @@ import (
 )
 
 func NewPostgresDB(dbURL string) (*gorm.DB, error) {
-	if !strings.Contains(dbURL, "search_path") {
-		dbURL += "?search_path=public"
-	}
-
 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 	if err != nil {
 		return nil, err
